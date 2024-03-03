958ae2cccb108ed85984b459f0e212ea84b10b0e
Amorais19 <alicemoraisams@gmail.com>
2024-03-03 12:47:39 -0300

    first

---
+++ crescente.php
@@ -0,0 +1,33 @@
+<?php
+    include 'dados.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>Arrays</title>
+</head>
+
+<body>
+    <header>
+        <h3 class="title">Utilizando Arrays em PHP</h3>
+        <a class="inicio" href="index.html">Início</a>
+    </header>
+    <main>
+        <div class="jumbotron">
+            <div class="container">
+                <h1 class="display-3">Manipulação de Arrays em PHP</h1>
+                <p>Exercício de utilização de Arrays em PHP</p>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+    $dados[] = array("nome" => "Alice Linda de Morrer", "idade" => 17, "estilo" => "Rock");
+    $dados[] = array("nome" => "Emanu", "idade" => 43, "estilo" => "Funk");
+    $dados[] = array("nome" => "Coraline", "idade" => 9, "estilo" => "Classica");
+    $dados[] = array("nome" => "Ribeirão", "idade" => 24, "estilo" => "Rock");
+    $dados[] = array("nome" => "Fernanda de Noronhas", "idade" => 70, "estilo" => "Rock");
+    $dados[] = array("nome" => "Luís Gameplay", "idade" => 90, "estilo" => "Classica");
+?>
